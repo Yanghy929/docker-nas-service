f43fa0720ec44f2482e7f702049a42f93c550c66
Yanghy <freedom548@gmail.com>
2018-03-08 00:34:17 +0800

    增加配置文件，修改run.sh脚本

--- conf/run.sh
+++ conf/run.sh
@@ -1,17 +1,33 @@
 #!/bin/sh
 
-#替换window系统中配置文件的配置路径，如没有需要的可以注释掉。本人是将window系统的配置文件用onedrive进行共享公用，实现多台机器使用aria2下载，因此需要替换。
+#替换window系统中配置文件的配置路径，如没有需要的可以注释掉。本人是将window系统的配置文件用onedrive进行共享公用，实现多台机器使用aria2下载，因此需要替换配置行。
 
-downloaddir='dir=/root/downloads'
+downloaddir='dir=/root/downloads' #下载路径
 
-sessiondir1='input-file=/root/downloads/aria2.session'
+sessiondir1='input-file=/root/downloads/aria2.session'  #session文件路径
 
-sessiondir2='save-session=/root/downloads/aria2.session'
+sessiondir2='save-session=/root/downloads/aria2.session'  #session文件路径
 
-certdir='rpc-certificate=/root/cert/certificate.pem'
+certdir='rpc-certificate=/root/cert/certificate.pem'   #证书路径
 
-keydir='rpc-private-key=/root/cert/certificate.key'
+keydir='rpc-private-key=/root/cert/certificate.key'  #私钥路径
 
-sed -e "/dir=/ c${downloaddir}" -e "/input-file=/ c${sessiondir1}" -e "/save-session=/ c${sessiondir2}" -e "/rpc-certificate=/ c${certdir}" -e "/rpc-private-key=/ c${keydir}"  /root/aria2/tmp/aria2.conf > /root/aria2/aria2.conf
+allocation='file-allocation=trunc' 
 
-/usr/bin/aria2c --conf-path /root/aria2/aria2.conf
+dhtpath='dht-file-path=/root/aria2/dht.dat'  #dht文件
+
+
+if [ -f "/root/aria2/tmp/aria2.conf" ]; then
+  sed -e "/dir\=/ c${downloaddir}" -e "/input-file\=/ c${sessiondir1}" -e "/save-session\=/ c${sessiondir2}"  -e "/rpc-certificate\=/ c${certdir}" -e "/rpc-private-key\=/ c${keydir}" -e "/file-allocation\=/ c${allocation}" -e "$ a${dhtpath}" /root/aria2/tmp/aria2.conf > /root/aria2/aria2.conf 
+fi	
+	
+if [ ! -f "/root/downloads/aria2.session" ]; then
+  touch /root/downloads/aria2.session
+fi
+
+if [ ! -f "/root/aria2/dht.dat" ]; then
+  touch /root/aria2/dht.dat
+fi
+
+
+/usr/bin/aria2c --conf-path=/root/aria2/aria2.conf
--- dockerfile
+++ dockerfile
@@ -1,26 +1,26 @@
-FROM alpine:edge
+FROM alpine
 
 LABEL maintainer="yu929" version="1.0"
 
-RUN apk update \
-      && apk --no-cache upgrade \
-      && apk add --no-cache aria2 \
+RUN apk add --no-cache aria2 \
       && mkdir -p /root/aria2/tmp \
       && mkdir -p /root/downloads \
-      && mkdir -p /root/cert \
+      && mkdir -p /root/cert
       
 
+COPY conf/aria2.conf /root/aria2/aria2.conf
 COPY conf/run.sh /root/run.sh
 
 RUN chmod +x /root/run.sh
 
+VOLUME /root/aria2/
 VOLUME /root/aria2/tmp/
 VOLUME /root/downloads/
 VOLUME /root/cert/
 
 EXPOSE 6800
-EXPOSE 6881
-EXPOSE 6882
+EXPOSE 6900
+EXPOSE 6999
 
 CMD /root/run.sh
 
